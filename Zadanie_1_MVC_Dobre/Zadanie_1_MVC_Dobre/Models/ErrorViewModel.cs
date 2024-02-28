e7394a19eb4c1709c5f9a1caaacf281f995944fb
Emil6011 <73350365+Emil6011@users.noreply.github.com>
2024-02-28 01:28:10 +0100

    Delete Zadanie_1_MVC_Dobre directory

--- Zadanie_1_MVC_Dobre/Zadanie_1_MVC_Dobre/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Zadanie_1_MVC_Dobre.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
